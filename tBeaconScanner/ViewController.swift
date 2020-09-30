b9f94a9e19db34514b67ee9b128fe5d5192a9745
wujidadi <wujidadi@gmail.com>
2020-09-30 16:40:18 +0800

    Initial Commit

---
+++ tBeaconScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tBeaconScanner
+//
+//  Created by 張成崗 on 2020/9/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
