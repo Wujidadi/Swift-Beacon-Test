4e25dc132853d828404cfd092f767a84337aefe6
wujidadi <wujidadi@gmail.com>
2020-10-03 11:50:33 +0800

    修改基本測試 code

--- tBeaconScanner/ViewController.swift
+++ tBeaconScanner/ViewController.swift
@@ -3,16 +3,16 @@ import CoreLocation
 
 class ViewController: UIViewController
 {
-    var locationManager: CLLocationManager!
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var msgLabel: UILabel!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
         monitorBeacons()
     }
 
@@ -20,70 +20,83 @@ class ViewController: UIViewController
     {
         if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self)
         {
-            let proximityUUID = UUID(uuidString: "0A4D8B73-7F74-4A83-B2CA-4FE84E870426")
+            let uuid = UUID(uuidString: "0A4D8B73-7F74-4A83-B2CA-4FE84E870426")
             let beaconId = "TUT"
-            let region = CLBeaconRegion(proximityUUID: proximityUUID!, identifier: beaconId)
+            let region = CLBeaconRegion(proximityUUID: uuid!, identifier: beaconId)
             locationManager.startMonitoring(for: region)
+            locationManager.startRangingBeacons(in: region)
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion)
-    {
-        print("enter region")
-        
-        if CLLocationManager.isRangingAvailable()
-        {
-            locationManager.startMonitoring(for: region as! CLBeaconRegion)
-            locationManager.startRangingBeacons(in: region as! CLBeaconRegion)
-        }
+    // func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion)
+    // {
+    //     print("enter region")
 
-        let content = UNMutableNotificationContent()
-        content.title = "注意"
-        content.subtitle = "小明就在你身邊"
-        content.badge = 1
-        content.sound = UNNotificationSound.default
+    //     if CLLocationManager.isRangingAvailable()
+    //     {
+    //         locationManager.startRangingBeacons(in: region as! CLBeaconRegion)
+    //     }
 
-        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: nil)
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    //     let content = UNMutableNotificationContent()
+    //     content.title = "注意"
+    //     content.subtitle = "小明就在你身邊"
+    //     content.badge = 1
+    //     content.sound = UNNotificationSound.default
 
-        if CLLocationManager.isRangingAvailable()
-        {
-            locationManager.startMonitoring(for: region as! CLBeaconRegion)
-            locationManager.startRangingBeacons(in: region as! CLBeaconRegion)
-        }
-    }
+    //     let request = UNNotificationRequest(identifier: "notification", content: content, trigger: nil)
+    //     UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
 
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion)
-    {
-        print("exit region")
+    //     if CLLocationManager.isRangingAvailable()
+    //     {
+    //         locationManager.startMonitoring(for: region as! CLBeaconRegion)
+    //         locationManager.startRangingBeacons(in: region as! CLBeaconRegion)
+    //     }
+    // }
 
-        locationManager.stopRangingBeacons(in: region as! CLBeaconRegion)
-    }
+    // func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion)
+    // {
+    //     print("exit region")
+
+    //     locationManager.stopRangingBeacons(in: region as! CLBeaconRegion)
+    // }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion)
     {
+        let currentDate = Date()
+        let dataFormatter = DateFormatter()
+        dataFormatter.locale = Locale(identifier: "zh_Hant_TW")
+        dataFormatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
+        let dateString = dataFormatter.string(from: currentDate)
+
+        // print(beacons)
+
         if beacons.count > 0
         {
-            let nearestBeacon = beacons[0]
-            print(nearestBeacon.proximityUUID, nearestBeacon.major, nearestBeacon.minor)
+            let beacon = beacons[0]
+            var beaconString = "[\(dateString)] UUID: \(beacon.proximityUUID), Major: \(beacon.major), Minor: \(beacon.minor)"
             
-            switch nearestBeacon.proximity
-            {
-                case .immediate:
-                    print("immediate")
+            // switch beacon.proximity
+            // {
+            //     case .immediate:
+            //         beaconString += ", Proximity: immediate"
 
-                case .near:
-                    print("near")
+            //     case .near:
+            //         beaconString += ", Proximity: near"
 
-                case .far:
-                    print("far")
+            //     case .far:
+            //         beaconString += ", Proximity: far"
 
-                case .unknown:
-                    print("unknown")
+            //     case .unknown:
+            //         beaconString += ", Proximity: unknown"
 
-                @unknown default:
-                    print("@unknown default")
-            }
+            //     @unknown default:
+            //         beaconString += ", Proximity: @unknown default"
+            // }
+
+            beaconString += ", RSSI: \(beacon.rssi)"
+
+            msgLabel.text = beaconString
+            print(beaconString)
         }
     }
 }
